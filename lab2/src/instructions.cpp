736cfd55912871b0db5c1333cc7f31463fd5f9b9
DmitryMustk <dmutrymust@gmail.com>
2023-11-06 06:47:45 +0700

    add instruction_file parser

---
+++ lab2/src/instructions.cpp
@@ -0,0 +1,38 @@
+#include "instructions.h"
+
+#include <fstream>
+#include <sstream>
+
+Instructions::Instructions(const std::string& filename) {
+    std::ifstream input(filename);
+    if (!input.is_open())
+        throw std::runtime_error("Can not open the instructions file");
+
+    std::string line;
+    while (std::getline(input, line)) {
+        std::stringstream str_stream(line);
+
+        std::string converter;
+        str_stream >> converter;
+
+        if (converter != "mute" && converter != "mix" && converter != "nightcore") {
+            input.close();
+            throw std::runtime_error("Wrong instructions file format, please use -h to get help");
+        }
+
+        std::vector<std::string> args;
+        size_t num_args = 2;
+
+        for (size_t i = 0; i < num_args; ++i) {
+            std::string tmp;
+            if (!(str_stream >> tmp)) {
+                input.close();
+                throw std::runtime_error("Wrong instructions file format, please use -h to get help");
+            }
+            args.push_back(tmp);
+        }
+
+        data.push_back(Instruction{converter, args});
+    }
+    input.close();
+}
---
+++ lab2/src/instructions.h
@@ -0,0 +1,19 @@
+#ifndef SOUNDPROCESSOR_INSTRUCTIONS_H
+#define SOUNDPROCESSOR_INSTRUCTIONS_H
+#include <string>
+#include <vector>
+
+class Instructions {
+   public:
+    struct Instruction {
+        std::string converter;
+        std::vector<std::string> args;
+    };
+    std::vector<Instruction> data;
+
+    Instructions(const std::string& filename);
+
+   private:
+};
+
+#endif  //SOUNDPROCESSOR_INSTRUCTIONS_H
